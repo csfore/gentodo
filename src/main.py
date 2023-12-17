41ce8fcaa35898c2afa99cdf6bac5b2828723db8
Christopher Fore <csfore@posteo.net>
2023-12-17 17:41:01 -0500

    Remove Gentodo comment

--- src/main.py
+++ src/main.py
@@ -4,128 +4,12 @@
 
 __version__ = '0.0.5'
 
-#import json
 import os
 import gentodo
-#import parser
+import bugzilla
 import argparse
-#from gentodo.parser import setup_parser
 
-#STORAGE_DIR = os.path.expanduser("~/.local/share/todo")
-#TODO_FILE = os.path.join(STORAGE_DIR, "todo.json")
-#
-#class Gentodo:
-#
-#    def __init__(self):
-#        self.data = self.read()
-#        self.longest = self.calc_length()
-#
-#    def calc_length(self):
-#        """Calculates the longest title in the todo list"""
-#        longest = 0
-#
-#        for todo_id in self.data:
-#            # Edge case in case someone doesn't put anything in
-#            if self.data[todo_id]['title'] == None:
-#                continue
-#            if len(self.data[todo_id]['title']) > longest:
-#                longest = len(self.data[todo_id]['title'])
-#    
-#        return longest
-#
-#
-#    def read(self):
-#        with open(TODO_FILE, "r") as todo:
-#            return json.load(todo)
-#    
-#
-#    def write(self):
-#        with open(TODO_FILE, "w") as todo:
-#            json.dump(self.data, todo, indent=4)
-#
-#
-## TODO:
-## Fix long titles breaking the output
-## TODO:
-## Implement a form of wrapping
-#def show_todo(args, gentodo):
-#    """Shows the items to do"""
-#    spaces = gentodo.longest + 2
-#    hspaces = (spaces // 2) + 3
-#
-#    # Verbose output
-#    if args.verbose:
-#        print("{} | {:<20}| {}".format("ID", "Title", "Detail"))
-#        print("{}".format("─" * 50))
-#        for key in gentodo.data:
-#            print(f"{key} │ {gentodo.data[key]['title']:<20}│ {gentodo.data[key]['details']}")
-#    elif args.brief:
-#        print("Title".rjust(hspaces))
-#        print("{}".format("─" * spaces))
-#        for key in gentodo.data:
-#            print(f"{gentodo.data[key]['title']}")
-#    else:
-#        print("{}| {}".format("Title".ljust(spaces), "Details"))
-#        print("{}".format("─" * int(48*1.5)))
-#        for key in gentodo.data:
-#            print("{}| {}".format(gentodo.data[key]['title'].ljust(spaces), gentodo.data[key]['details']))
-#
-#
-#def add_item(args, gentodo):
-#    """Adds an item to the todo list"""
-#    if os.path.exists(TODO_FILE) and os.path.getsize(TODO_FILE) > 0:
-#        #data = read_storage()
-#        newest_id = 0 if len(gentodo.data.keys()) == 0 else int(list(gentodo.data.keys())[-1])
-#    else:
-#        data = {}
-#        newest_id = 0
-#    
-#    if args.details is None:
-#        args.details = ["No", "details"]
-#
-#
-#    gentodo.data[newest_id + 1] = { 
-#        "title": " ".join(args.title),
-#        "details": " ".join(args.details)
-#    }
-#
-#    gentodo.write()
-#    print(f"Added: {' '.join(args.title)} | {' '.join(args.details)}")
-#
-#
-#def rm_item(args, gentodo):
-#    """Removes an item from the todo list by ID"""
-#    if os.path.exists(TODO_FILE) and os.path.getsize(TODO_FILE) > 0:
-#        gentodo.data.pop("{0}".format(args.id))
-#        gentodo.write()
-#        
-#
-#def item_count(args, gentodo):
-#    """Tallies up the amount of items in the list"""
-#        
-#    remaining = len(gentodo.data.keys())
-#    print(f"Items remaining: {remaining}")
-#
-#
-#def edit_item(args, gentodo):
-#    """Edits an item entry"""
-#    gentodo.data[args.id]['title'] = " ".join(args.title)
-#    gentodo.data[args.id]['details'] = " ".join(args.details)
-#    
-#    gentodo.write()
-#
-#
-#def search_items(args, gentodo):
-#    print(f"Searching for: {args.term}\n\n")
-#    print("ID | Title")
-#    print("{}".format("─" * int(gentodo.longest/2)))
-#    for key in gentodo.data:
-#        for val in gentodo.data[key]:
-#            if args.term in gentodo.data[key][val]:
-#                print("{} | {}".format(key, gentodo.data[key][val]))
-#
-# TODO:
-# Break this into a separate file to prevent a huge single file
+
 def setup_parser():
     """Sets up the parser and adds arguments"""
 
@@ -164,6 +48,11 @@ def setup_parser():
     search_parser = subparsers.add_parser('search', help="Search for an item", usage="todo search [term]")
     search_parser.add_argument('term')
     search_parser.set_defaults(func=gentodo.search_items)
+
+    # Parser for bugzilla
+    bug_parser = subparsers.add_parser('bug', help="Interface with Bugzilla", usage="gentodo bug ...")
+    bug_parser.add_argument('-s', help="Search bugzilla")
+    bug_parser.
 
     return parser
 
