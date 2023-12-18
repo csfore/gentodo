a105fa908e2be7e4e219ef834cbe7e8d9dee70ae
Christopher Fore <csfore@posteo.net>
2023-12-18 15:10:10 -0500

    0.1.0 release
    First "stable" version released, cleaned up help messages.

--- src/gentodo/main.py
+++ src/gentodo/main.py
@@ -2,8 +2,6 @@
 # (c) 2023 Christopher Fore
 # This code is licensed under the GPLv3 license (see LICENSE for details)
 
-__version__ = '0.0.5'
-
 import os
 from gentodo import cli
 import bugzilla
@@ -14,7 +12,7 @@ def setup_parser():
     """Sets up the parser and adds arguments"""
 
     # Main parser
-    parser = argparse.ArgumentParser(usage="todo <command> [-h]")
+    parser = argparse.ArgumentParser(usage="gentodo <command> [-h]")
     parser.add_argument('--version', action='version', version='0.0.1')
     parser.add_argument('-v', '--verbose', action='store_true',help="Be descriptive about output (i.e. show item IDs)")
     parser.add_argument('-b', '--brief', action='store_true', help="Show just the title")
@@ -23,13 +21,13 @@ def setup_parser():
     subparsers = parser.add_subparsers(help='sub-command help', metavar='')
     
     # Parser for adding items
-    add_parser = subparsers.add_parser('add', help="Add an item to your todo list", usage="todo add [-h] -t <title> -d <details>")
+    add_parser = subparsers.add_parser('add', help="Add an item to your todo list", usage="gentodo add [-h] -t <title> -d <details>")
     add_parser.add_argument('-t', '--title', nargs='+', help="Item to add to your todo list")
     add_parser.add_argument('-d', '--details', nargs='+', help="Add some information to your item")
     add_parser.set_defaults(func=cli.add_item)
     
     # Parser for removing items
-    del_parser = subparsers.add_parser('del', help="Remove an item from your todo list", usage="todo rm <id>")
+    del_parser = subparsers.add_parser('del', help="Remove an item from your todo list", usage="gentodo del <id>")
     del_parser.add_argument('id', help="ID to remove from your todo list")
     del_parser.set_defaults(func=cli.rm_item)
 
@@ -38,14 +36,14 @@ def setup_parser():
     count_parser.set_defaults(func=cli.item_count)
 
     # Parser for editing
-    edit_parser = subparsers.add_parser('edit', help="Edit an item by id", usage="todo edit <id> -t <title> -d <details>")
+    edit_parser = subparsers.add_parser('edit', help="Edit an item by id", usage="gentodo edit <id> -t <title> -d <details>")
     edit_parser.add_argument('id')
     edit_parser.add_argument('-t', '--title', nargs='+', help="New title")
     edit_parser.add_argument('-d', '--details', nargs='+', help="New details")
     edit_parser.set_defaults(func=cli.edit_item)
 
     # Parser for searching
-    search_parser = subparsers.add_parser('search', help="Search for an item", usage="todo search [term]")
+    search_parser = subparsers.add_parser('search', help="Search for an item", usage="gentodo search [term]")
     search_parser.add_argument('term')
     search_parser.set_defaults(func=cli.search_items)
 
